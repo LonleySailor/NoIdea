b7dfa8383c105bfd9ec12cf875cbcf0d341f061f
KingOfNothingness <161038819+KingOfNothingness@users.noreply.github.com>
2024-05-05 19:17:11 +0200

    fixed: Sever not responding correctly

--- books/books.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.books = void 0;
-exports.books = [
-    { id: 1, title: 'Book 1', author: 'Author 1', genre: 'Genre 1' },
-    { id: 2, title: 'Book 2', author: 'Author 2', genre: 'Genre 2' },
-    { id: 3, title: 'Book 3', author: 'Author 3', genre: 'Genre 3' }
-];
